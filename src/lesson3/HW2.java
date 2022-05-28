476488f4ff0676226b0a8591d5d53f5cd4b9e54d
Andrew <franz.andrewn@gmail.com>
2022-05-28 20:01:39 +0300

    Added lesson3 hw tasks

---
+++ src/lesson3/HW2.java
@@ -0,0 +1,12 @@
+package lesson3;
+
+/*
+Внутри класса HW2 написать метод с именем repeatString, который принимает строку s и целое число num как аргументы
+и выводит строку s на экран num раз (нет возвращаемого значения)
+Например, если в psvm вызвать repeatString("asd",2), то в консоли должно быть "asdasd"
+ */
+public class HW2 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/lesson3/HW3.java
@@ -0,0 +1,15 @@
+package lesson3;
+
+/*
+Внутри класса HW3 написать метод под именем oneOrLessSubst, который принимает строку original и строку target
+Метод должен возвращать true, если target встречается внутри original не больше 1 раза
+и false в обратном случае
+Например для строк original="asdasdasd" и target="asd" он должен вернуть false
+Для строк original="привет, это Java программа" и target=" это Java " он должен вернуть true
+В psvm проверить работу метода на примерах или любых других строках
+ */
+public class HW3 {
+    public static void main(String[] args) {
+
+    }
+}
